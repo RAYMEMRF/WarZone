712906a6f9ce61993d1b719735dfc710c5974600
Richard YMDesk <richard_aym@openmailbox.org>
2016-06-09 08:45:48 -0400

    updating...

--- PHP/Tera/admin/resources/includes/footer.php
+++
@@ -1,7 +0,0 @@
-    </div> <!-- /container -->
-    <div id="footer">
-      <div class="container">
-        <p class="text-muted credit text-right">Este sitio web fue desarrollado por <a target="_blank" href="http://www.cesca-software.com/web/" target="_blank">cesca-software.com</a> &copy; 2013</a>.</p>
-      </div>
-    </div>
-</body></html>
--- PHP/Tera/admin/resources/includes/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Start or retrieve the session
-session_start();
-//COMPRUEBA QUE EL USUARIO ESTA AUTENTICADO
-if(!isset($_SESSION['uid']))
-{
-    echo "<SCRIPT LANGUAGE='javascript'>location.href = '".HTTP_ROOT."/login.php'</SCRIPT>";
-}
-?>
